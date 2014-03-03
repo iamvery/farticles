9ad866f8c8d3326fc828c242fa6ab3befe6dcaf0
Jay Hayes <ur@iamvery.com>
2014-03-03 13:07:08 -0600

    Add Article model

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20140303190531) do
+
+  create_table "articles", force: true do |t|
+    t.string "name", null: false
+  end
 
 end
