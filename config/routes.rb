57304c163ca5365b791f6c61e81c93c3b42e984c
Jay Hayes <ur@iamvery.com>
2014-03-04 07:20:22 -0600

    Add :as to routes for proper url helpers
    * Without this the URL helper generation wasn't smart enough to figure
      out the "v*" prefix. Now helpers generate as "api_v1_articles_path"

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Farticles::Application.routes.draw do
   namespace :api do
-    scope module: :v1, constraints: ApiVersionConstraint.new(version: 1) do
+    scope module: :v1, as: :v1, constraints: ApiVersionConstraint.new(version: 1) do
       resources :articles
     end
 
-    scope module: :v2 do
+    scope module: :v2, as: :v2 do
       resources :articles
     end
   end
