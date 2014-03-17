d5dcd99b2f9051448e8b52012e82c2e210633df8
Jay Hayes <ur@iamvery.com>
2014-03-16 21:51:32 -0400

    Add categories table to db

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140303190531) do
+ActiveRecord::Schema.define(version: 20140317015004) do
 
   create_table "articles", force: true do |t|
+    t.string "name", null: false
+  end
+
+  create_table "categories", force: true do |t|
     t.string "name", null: false
   end
 
